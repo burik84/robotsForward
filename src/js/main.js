5cc5b161c8591a214f4176c271a9a4dabc09d844
burik84 <AVburik84@gmail.com>
2018-10-23 22:51:09 +0300

    Подготовка проекта. подключение шрифтов, js, html, scss

---
+++ src/js/main.js
@@ -0,0 +1,18 @@
+// Определения списка файлов, которые необходимо включить
+// Импортируем jQuery
+//= ../../node_modules/jquery/dist/jquery.js
+// Импортируем Popper
+//= ../../node_modules/popper.js/dist/umd/popper.js
+
+// Импортируем необходимые js-файлы Bootstrap 4
+//= ../../node_modules/bootstrap/dist/js/bootstrap.js
+
+// Импортируем bootstrap-validator
+
+
+// Импортируем select2
+
+
+
+// Импортируем другие js-файлы
+//= my.js

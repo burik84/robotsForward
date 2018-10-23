8ffca5573f189fe668077a0c86e172c3dd3219ed
burik84 <AVburik84@gmail.com>
2018-10-24 00:32:47 +0300

    Подключение оставшихся пакетов к проекту. проблема - как использовать теперь fontawesome5 через scss и шрифты?

--- src/js/main.js
+++ src/js/main.js
@@ -1,4 +1,4 @@
-// Определения списка файлов, которые необходимо включить
+// Определения списка файлов, которые необходимо включить.
 // Импортируем jQuery
 //= ../../node_modules/jquery/dist/jquery.js
 // Импортируем Popper
@@ -7,12 +7,14 @@
 // Импортируем необходимые js-файлы Bootstrap 4
 //= ../../node_modules/bootstrap/dist/js/bootstrap.js
 
-// Импортируем bootstrap-validator
+// Импортируем bootstrap-validator.js
+//= ../../node_modules/bootstrap-validator/dist/validator.js
 
+// Импортируем select2.js
+//= ../../node_modules/select2/dist/js/select2.full.js
 
-// Импортируем select2
-
-
+// Импортируем wow.js
+//= ../../node_modules/wow.js/dist/wow.js
 
 // Импортируем другие js-файлы
 //= my.js

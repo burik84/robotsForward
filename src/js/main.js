8db1d2a81fb1e8e625c87f77c19d5de16a792974
burikAV <burikav@yandex.ru>
2019-03-02 09:51:01 +0300

    Удалил toltip и popover

--- src/js/main.js
+++ src/js/main.js
@@ -1,8 +1,6 @@
 // Определения списка файлов, которые необходимо включить.
 // Импортируем jQuery
 //= ../../node_modules/jquery/dist/jquery.js
-// Импортируем Popper
-//= ../../node_modules/popper.js/dist/umd/popper.js
 
 // Импортируем необходимые js-файлы Bootstrap 4
 //= ../../node_modules/bootstrap/dist/js/bootstrap.js

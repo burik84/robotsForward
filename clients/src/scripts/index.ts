65cbfde2bd8a5e0493c060dbe002e455209d71f3
burik84 <avburik84@gmail.com>
2022-04-21 17:24:55 +0300

    feat: add fontawesome

--- clients/src/scripts/index.ts
+++ clients/src/scripts/index.ts
@@ -1,8 +1,8 @@
+import 'bootstrap';
 import { insertHTML, warning } from './services/insertHTML';
 import { SHOWWARNING } from './services/service';
 import '../styles/styles.scss';
 
-console.log('Hello World!');
 insertHTML();
 
 const header = document.querySelector('#header .container');

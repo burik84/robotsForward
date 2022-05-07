ba99e4db51b684f0663ec2c6e9a4e3d86cb3eb7d
burik84 <avburik84@gmail.com>
2022-05-07 23:02:34 +0300

    fix: add modal to script

--- clients/src/scripts/table.ts
+++ clients/src/scripts/table.ts
@@ -1,8 +1,12 @@
+/* eslint-disable no-unused-vars */
+/* eslint-disable import/no-relative-packages */
+import { Modal } from '../../node_modules/bootstrap/dist/js/bootstrap';
 import { insertHTML } from './services/insertHTML';
 import '../styles/pages/table.scss';
 
 insertHTML('table');
 
-// document.addEventListener('DOMContentLoaded', () => {
-//   scrollUp();
-// });
+document.addEventListener('DOMContentLoaded', () => {
+  const aboutModal = new Modal(document.getElementById('aboutus'));
+  const refuseModal = new Modal(document.getElementById('refusing'));
+});

a6e4b904d9e344d1c0f2bdc59dc60bc6ea57ac2c
burik84 <avburik84@gmail.com>
2022-05-13 00:28:06 +0300

    feat: add data

---
+++ clients/src/scripts/services/getData.ts
@@ -0,0 +1,25 @@
+import { URL, DATA } from './service';
+
+export const getSourceData = async () => {
+  const url = `${URL.ru}public/service/${DATA.source}`;
+  const data = await fetch(url).then((response) => response.json()).then((res) => {
+    console.log(res);
+    return res;
+  }).catch((error) => {
+    console.log('Something went wrong', error.message);
+  });
+  return data;
+};
+
+export const getWinnerData = {
+  table2019: async () => {
+    const url = `${URL.ru}public/table/${DATA.table2019}`;
+    const data = await fetch(url).then((response) => response.json()).then((res) => {
+      console.log(res);
+      return res;
+    }).catch((error) => {
+      console.log('Something went wrong', error.message);
+    });
+    return data;
+  },
+};
--- clients/src/scripts/services/insertHTML.ts
+++ clients/src/scripts/services/insertHTML.ts
@@ -15,5 +15,10 @@ const insertHTML = (page:string) => {
   main.insertAdjacentHTML('afterend', modal);
   // if (page !== 'error') body.insertAdjacentHTML('beforeend', scrollup);
 };
-const warning = '<p class="lead text-primary text-center mb-5 mx-3"><span class="badge rounded-pill bg-danger">Внимание!</span> <br> В 2022 году проведение соревнований не запланировано!</p>';
-export { insertHTML, warning };
+
+const iframeHTML = {
+  youtube: '<iframe class="pt-1 pt-sm-2" src="https://www.youtube.com/embed/GBCS0qtjDgU" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen aria-describedby="video-description"></iframe><p id="video-description" class="visually_hidden">Ролик представляет краткий обзор прошедших соревнований "Роботы вперёд" 19 мая 2019года. В соревнованиях приняли участие обучающиеся разных учреждений из г.Кингисепп, г.Ивангорода, Санкт-Петербурга, г.Сланцы и Волосовского района. Ребята смогли поучаствовать в трех категориях: «Царь горы», «Тягачи» и «Механическое сумо».</p>',
+  map: '<iframe src="https://yandex.ru/map-widget/v1/?um=constructor%3Abe645264758c32a012bf168fe8fc43a534a790c0c9d757808e7a6fbaf4253c0a&amp;source=constructor" title="Карта где мы расположены - г. Сланцы, ул. Кирова, д.16, эт.3"></iframe>',
+};
+
+export { insertHTML, iframeHTML };

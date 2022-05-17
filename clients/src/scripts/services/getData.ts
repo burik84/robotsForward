f64d631f9c124a4c9ab675c06d3331b6e60fbde3
burik84 <avburik84@gmail.com>
2022-05-17 22:54:12 +0300

    fix: del log

--- clients/src/scripts/services/getData.ts
+++ clients/src/scripts/services/getData.ts
@@ -2,24 +2,24 @@ import { URL, DATA } from './service';
 
 export const getSourceData = async () => {
   const url = `${URL.ru}public/service/${DATA.source}`;
-  const data = await fetch(url).then((response) => response.json()).then((res) => {
-    console.log(res);
-    return res;
-  }).catch((error) => {
-    console.log('Something went wrong', error.message);
-  });
+  const data = await fetch(url)
+    .then((response) => response.json())
+    .then((res) => res)
+    .catch((error) => {
+      console.log('Something went wrong', error.message);
+    });
   return data;
 };
 
 export const getWinnerData = {
-  table: async (year:string) => {
+  table: async (year: string) => {
     const url = `${URL.ru}public/table/win${year}.json`;
-    const data = await fetch(url).then((response) => response.json()).then((res) => {
-      console.log(res);
-      return res;
-    }).catch((error) => {
-      console.log('Something went wrong', error.message);
-    });
+    const data = await fetch(url)
+      .then((response) => response.json())
+      .then((res) => res)
+      .catch((error) => {
+        console.log('Something went wrong', error.message);
+      });
     return data;
   },
 };
